78b1ab963f7918131647b470dec5389095046898
ahami14 <ahnikkahamilton14@gmail.com>
2019-11-15 08:58:57 -0600

    Twilio logic in place

--- BudgetBuddy/Migrations/201911151438282_InitialMigration.Designer.cs
+++ BudgetBuddy/Migrations/201911151438282_InitialMigration.Designer.cs
@@ -7,13 +7,13 @@ namespace BudgetBuddy.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.0-preview2-19525-03")]
-    public sealed partial class savings : IMigrationMetadata
+    public sealed partial class InitialMigration : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(savings));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialMigration));
         
         string IMigrationMetadata.Id
         {
-            get { return "201911132003092_savings"; }
+            get { return "201911151438282_InitialMigration"; }
         }
         
         string IMigrationMetadata.Source
